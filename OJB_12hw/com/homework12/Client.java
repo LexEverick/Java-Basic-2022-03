aedb3db7f0853fbdb200c3f7377dc1d01a7c8da7
Alexander Abramov <aaeastlex@gmail.com>
2022-07-30 15:23:04 +0300

    Change date of birth to age

--- OJB_12hw/com/homework12/Client.java
+++ OJB_12hw/com/homework12/Client.java
@@ -3,7 +3,7 @@ package com.homework12;
 public class Client {
 
     private String name;
-    private int yearOfBirth = 1950 + (int) (Math.random() * 54);
+    private int yearOfBirth = 18 + (int) (Math.random() * 62);
 
     public Client(String name) {
         this.name = name;

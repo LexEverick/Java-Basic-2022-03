e4388cf252c3a1775ddde0ff51caeb9c7930126e
Alexander Abramov <aaeastlex@gmail.com>
2022-07-30 15:15:06 +0300

    Class Client at the pre-final stage

--- OJB_12hw/com/homework12/Client.java
+++ OJB_12hw/com/homework12/Client.java
@@ -1,12 +1,30 @@
 package com.homework12;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
 public class Client {
 
-    private static final String name = "";
-    private static final Calendar birthday = new GregorianCalendar();
+    private String name;
+    private int yearOfBirth = 1950 + (int) (Math.random() * 54);
+
+    public Client(String name) {
+        this.name = name;
+        //this.earOfBirth = earOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
 
 
 
